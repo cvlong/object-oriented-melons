4e9ea1c9195d5fc3baa21953c4f5f814153e6afd
cvlong <cvlong@gmail.com>
2016-04-15 13:05:32 -0700

    updated total price and added docstrings

--- melons.py
+++ melons.py
@@ -1,5 +1,3 @@
-"""This file should have our order classes in it."""
-
 class AbstractMelonOrder(object):
     """Generalized Information for International & Domestic Melon Orders"""
     
@@ -10,12 +8,31 @@ class AbstractMelonOrder(object):
         self.qty = qty
         self.shipped = False
 
-    def get_total(self):
-        """Calculate price."""
+
+    # def get_total(self):
+    #     """Calculate price."""
+
+    #     base_price = 5
+    #     total = (1 + self.tax) * self.qty * base_price
+    #     return total
+
+
+    def get_total_holiday(self):
+        """Calculate total price, higher price for international and Xmas melons"""
 
         base_price = 5
+
+        if self.species == "Christmas melon":
+            base_price = base_price * 1.5
+        
         total = (1 + self.tax) * self.qty * base_price
+        
+        if self.order_type == "international":
+            if self.qty < 10:
+                total = total + 3
+        
         return total
+
 
     def mark_shipped(self):
         """Set shipped to true."""
@@ -27,7 +44,9 @@ class DomesticMelonOrder(AbstractMelonOrder):
     """A domestic (in the US) melon order."""
 
     def __init__(self, species, qty):
+        """Initialize a domestic melon order"""
         super(DomesticMelonOrder, self).__init__(species, qty)
+        
         self.order_type = "domestic"
         self.tax = 0.08
 
@@ -40,7 +59,9 @@ class InternationalMelonOrder(AbstractMelonOrder):
     """An international (non-US) melon order."""
 
     def __init__(self, species, qty, country_code):
+        """Initialize an international melon order"""
         super(InternationalMelonOrder, self).__init__(species, qty)
+        
         self.order_type = "international"
         self.tax = 0.17
         self.country_code = country_code
